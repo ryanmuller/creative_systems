0cdbab9de87101d56b4b0abf26833a7087108e78
Ryan Muller <cognitionmachine@gmail.com>
2011-10-15 14:43:29 -0400

    added projects with basic specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111015040346) do
+ActiveRecord::Schema.define(:version => 20111015184014) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
